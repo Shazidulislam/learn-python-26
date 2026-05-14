e918c7e840883178c533951e1e4ca41cbc53f8db
Shazidulislam <shazidulislam910@gmail.com>
2026-05-14 22:42:16 +0600

    complete chatper 1

---
+++ Lecture3/04_escape_seq.py
@@ -0,0 +1,16 @@
+
+# ESCAPE SEQUENCE CHARACTERS
+
+a = "Shamim is a good boy but \nnot a bad boy"
+
+print(a)
+
+print("Hello\tWorld")
+print("Every \\ Body")
+
+print("Singl\'e Quote")
+print("Double \"Quote\" ")
+
+#  Carriage Return (লাইনের শুরুতে যায়)
+print("is start here.  \rLine ")
+print("Helloooo\rWorld")
